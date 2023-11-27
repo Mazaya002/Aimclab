b34f71341abbbe4002f53c8049cd2c245ab07a62
Mazaya Satrio <90241788+Mazaya002@users.noreply.github.com>
2023-11-27 16:06:52 +0100

    Communist

--- aimclab/js/btn.js
+++ aimclab/js/btn.js
@@ -1,3 +1,12 @@
 document.getElementById('registerBtn').addEventListener('click', function() {
     window.location.href = 'register.html'; 
 });
+
+document.getElementById('registerBtn').addEventListener('mouseover', function() {
+    this.style.transform = 'scale(1.1)';
+    this.style.transition = 'transform 0.3s ease';
+});
+
+document.getElementById('registerBtn').addEventListener('mouseout', function() {
+    this.style.transform = 'scale(1)';
+});
